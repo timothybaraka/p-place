cf3882996aea2404b82bcbb9c44f2e4418191995
timothybaraka <timothybaraka44@gmail.com>
2019-12-16 11:54:42 +0300

    Add icons for the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(){
-    $(#pizzalarge).click(function(){
-        $(.formlarge).hide();
-        $(#pizzalarge).toggle();
-        $(.formlarge).show();
-    });
+// $(document).ready(){
+//     $(#pizzalarge).click(function(){
+//         $(.formlarge).hide();
+//         $(#pizzalarge).toggle();
+//         $(.formlarge).show();
+//     });
 
-};
-alert("yoooo");
-function totalPrice(){
-    var selectedCrust=document.getElementById("crusts")
-    var CostOfCrust=selectedCrust.options[selectedCrust.selectedIndex].value;
-    var selectedTopping=document.getElementById("toppings");
-    var CostOfTopping=selectedTopping.options[selectedTopping.selectedIndex].value;
-    var selectedpizzaquant=document.getElementById("pizzaquantity").value;
+// };
+// alert("yoooo");
+// function totalPrice(){
+//     var selectedCrust=document.getElementById("crusts")
+//     var CostOfCrust=selectedCrust.options[selectedCrust.selectedIndex].value;
+//     var selectedTopping=document.getElementById("toppings");
+//     var CostOfTopping=selectedTopping.options[selectedTopping.selectedIndex].value;
+//     var selectedpizzaquant=document.getElementById("pizzaquantity").value;
 
-    var price=parseInt(CostOfCrust)+parseInt(CostOfTopping);
-    alert(CostOfCrust);
-}
+//     var price=parseInt(CostOfCrust)+parseInt(CostOfTopping);
+//     alert(CostOfCrust);
+// }
 
 
