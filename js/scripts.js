e403279fa2240a33ffc4ad87a4f5770c84281eb3
timothybaraka <timothybaraka44@gmail.com>
2019-12-16 14:44:32 +0300

    Add an onlick method to display the forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
-// $(document).ready(){
-//     $(#pizzalarge).click(function(){
-//         $(.formlarge).hide();
-//         $(#pizzalarge).toggle();
-//         $(.formlarge).show();
-//     });
+$(document).ready(function(){
+    $("#pizzalarge").click(function(){
+    $(".formlarge").hide();
+    $("#pizzalarge").toggle();
+    $(".formlarge").show();
+    });
+    
+    $("#pizzamedium").on("click", function(){
+        $(".formmedium").hide();
+        $("#pizzamedium").toggle();
+        $(".formmedium").show();
+        });
 
-// };
-// alert("yoooo");
-// function totalPrice(){
-//     var selectedCrust=document.getElementById("crusts")
-//     var CostOfCrust=selectedCrust.options[selectedCrust.selectedIndex].value;
-//     var selectedTopping=document.getElementById("toppings");
-//     var CostOfTopping=selectedTopping.options[selectedTopping.selectedIndex].value;
-//     var selectedpizzaquant=document.getElementById("pizzaquantity").value;
+        $("#pizzasmall").on("click", function(){
+            $(".formsmall").hide();
+            $("#pizzasmall").toggle();
+            $(".formsmall").show();
+            }); 
+});
 
-//     var price=parseInt(CostOfCrust)+parseInt(CostOfTopping);
-//     alert(CostOfCrust);
-// }
-
-
+function totalPrice(){
+    var selectedCrust=document.getElementById("crusts")
+    var CostOfCrust=selectedCrust.options[selectedCrust.selectedIndex].value;
+    var selectedTopping=document.getElementById("toppings");
+    var CostOfTopping=selectedTopping.options[selectedTopping.selectedIndex].value;
+    var selectedpizzaquant=document.getElementById("pizzaquantity").value;
+    
+    var price=parseInt(CostOfCrust)+parseInt(CostOfTopping);
+    alert(CostOfCrust);
+    }
